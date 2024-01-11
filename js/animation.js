04637b416744bbeaddc0e6c5fffdad02686c6087
Gonzalo Gabriel Ramirez <gonzalogabrielramirez@outlook.com.ar>
2024-01-11 20:40:25 -0300

    Header Fixed v4

--- js/animation.js
+++ js/animation.js
@@ -33,11 +33,13 @@ const listLi         = document.getElementsByClassName("header__li");
 function showMenu(){
   nav.style.right = "0px";
   backgroundMenu.style.display = "block";
+  hamburguerIcon.style.display = "none"
 };
 
 function hiddenMenu(){
   nav.style.right = "-100%"
   backgroundMenu.style.display = "none";
+  hamburguerIcon.style.display = "block"
 }
 
 hamburguerIcon.addEventListener("click", showMenu);
