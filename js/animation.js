b236125d8372b9ee23293a2fdcf530831f314900
Gonzalo Gabriel Ramirez <gonzalogabrielramirez@outlook.com.ar>
2024-01-13 16:17:39 -0300

    Header Fixed v14

--- js/animation.js
+++ js/animation.js
@@ -3,11 +3,11 @@ function animationBars(){
     const barsFront = document.getElementsByClassName("bar__front");
     const chartBart = document.getElementsByClassName("chart__bar")[0];
     const title     = document.getElementsByClassName("name__title")[0];
-    // const subtitle  = document.getElementsByClassName("name__subtitle")[0];
+    const subtitle  = document.getElementsByClassName("name__subtitle")[0];
 
     chartBart.classList.add("chart__bar--active");
     title.classList.add("name__title--active");
-    // subtitle.classList.add("name__subtitle--active");
+    subtitle.classList.add("name__subtitle--active");
 
     Array.from(barsBack).forEach((bar, i) => {
         bar.classList.add(`bar__back--${i+1}`);
